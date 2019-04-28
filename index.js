c19fb7e317c9b7133cc0736afa2a41520afbe1ec
MichaelSieghartsleitenr <s1510456035@students.fh-hagenberg.at>
2019-04-28 17:14:18 +0200

    test

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const express = require('express')
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-});
+// app.get('/', (req, res) => {
+//   res.send('Hello World!')
+// });
 
 app.listen(8000, () => {
   console.log('ORF-Chatbot listening on port 8000!')
