b2d05cd92bc83eb7a6e3a0bae7282549ab8d23f0
MichaelSieghartsleitenr <s1510456035@students.fh-hagenberg.at>
2019-04-28 18:45:00 +0200

    fulfillment agent

--- index.js
+++ index.js
@@ -1,16 +1,33 @@
-const express = require('express')
-const path = require('path')
-const PORT = process.env.PORT || 5000
+const express = require('express');
+const path = require('path');
+const PORT = process.env.PORT || 5000;
+const {WebhookClient} = require('dialogflow-fulfillment');
+const {Card, Suggestion} = require('dialogflow-fulfillment');
+
 const server = express();
 server.use(express.static(path.join(__dirname, 'public')));
 
 server.get('/', (req, res) => res.send('page'));
-server.post('/orf-data', (req, res) => res.json({
-    "speech": "das ist ein test",
-    "displayText": "das ist ein test",
-    "source": 'get-movie-details'
-}));
+server.post('/orf-data', (req, res) => {
+    const agent = new WebhookClient({ request, response });
+console.log('Dialogflow Request headers: ' + JSON.stringify(request.headers));
+console.log('Dialogflow Request body: ' + JSON.stringify(request.body));
 
+function welcome(agent) {
+    agent.add(`Willkommen zum ORF Chatbot!`);
+}
+
+function fallback(agent) {
+    agent.add(`Das habe ich nicht verstanden`);
+}
+
+let intentMap = new Map();
+intentMap.set('Default Welcome Intent', welcome);
+intentMap.set('Default Fallback Intent', fallback);
+// intentMap.set('your intent name here', yourFunctionHandler);
+// intentMap.set('your intent name here', googleAssistantHandler);
+agent.handleRequest(intentMap);
+});
 
 
 server.listen(PORT, () => console.log(`Listening on ${ PORT }`));
