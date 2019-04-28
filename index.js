7425dddd69c8533e8b676d6fd4ec0ba7bc3340bb
MichaelSieghartsleitenr <s1510456035@students.fh-hagenberg.at>
2019-04-28 17:26:27 +0200

    test

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
 const express = require('express')
-const app = express();
+const path = require('path')
+const PORT = process.env.PORT || 5000
 
-// app.get('/', (req, res) => {
-//   res.send('Hello World!')
-// });
-
-app.listen(8000, () => {
-  console.log('new ORF-Chatbot listening on port 8000!')
-});
+express()
+    .use(express.static(path.join(__dirname, 'public')))
+    .get('/', (req, res) => res.send('page'))
+.get('/cool', (req, res) => res.send('cool'))
+.listen(PORT, () => console.log(`Listening on ${ PORT }`))
