5330bf3ca3de7bdf540a3838229e6df97a93258c
MichaelSieghartsleitenr <s1510456035@students.fh-hagenberg.at>
2019-04-28 19:05:04 +0200

    body parser, url encode

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const path = require('path');
 const PORT = process.env.PORT || 5000;
 const {WebhookClient} = require('dialogflow-fulfillment');
@@ -6,27 +7,34 @@ const {Card, Suggestion} = require('dialogflow-fulfillment');
 
 const server = express();
 server.use(express.static(path.join(__dirname, 'public')));
+server.use(bodyParser.urlencoded({
+    extended: true
+}));
+server.use(bodyParser.json());
+
+
 
 server.get('/', (req, res) => res.send('page'));
-server.post('/orf-data', (req, res) => {
-    const agent = new WebhookClient({ req, res });
-console.log('Dialogflow Request headers: ' + JSON.stringify(req.headers));
-console.log('Dialogflow Request body: ' + JSON.stringify(req.body));
+server.post('/orf-data', (request, response) => {
+    const agent = new WebhookClient({ request, response });
+    console.log('Dialogflow Request headers: ' + JSON.stringify(request.headers));
+    console.log('Dialogflow Request body: ' + JSON.stringify(request.body));
 
-function welcome(agent) {
-    agent.add(`Willkommen zum ORF Chatbot!`);
-}
+    function welcome(agent) {
+        agent.add(`Willkommen zum ORF Bot!`);
+    }
 
-function fallback(agent) {
-    agent.add(`Das habe ich nicht verstanden`);
-}
+    function fallback(agent) {
+        agent.add(`I didn't understand`);
+        agent.add(`I'm sorry, can you try again?`);
+    }
 
-let intentMap = new Map();
-intentMap.set('Default Welcome Intent', welcome);
-intentMap.set('Default Fallback Intent', fallback);
-// intentMap.set('your intent name here', yourFunctionHandler);
-// intentMap.set('your intent name here', googleAssistantHandler);
-agent.handleRequest(intentMap);
+    let intentMap = new Map();
+    intentMap.set('Default Welcome Intent', welcome);
+    intentMap.set('Default Fallback Intent', fallback);
+    // intentMap.set('your intent name here', yourFunctionHandler);
+    // intentMap.set('your intent name here', googleAssistantHandler);
+    agent.handleRequest(intentMap);
 });
 
 
